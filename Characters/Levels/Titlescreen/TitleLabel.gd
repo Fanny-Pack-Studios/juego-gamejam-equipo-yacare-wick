aabde2e89e72f381843dbca11c01e8cc326477a9
Hernan-dh <63477196+Hernan-dh@users.noreply.github.com>
2023-02-03 23:45:45 -0300

    Agregar músicas a pantallas y cambios pequeños
    Agregué música a la Intro, Outro y al selector de pilotos (hay que ver si están bien).
    Creé la pantalla Title Screen (hay que hacer que dirija a la Intro).
    Agregué un textito al Game Over (hay que ver si les gusta).
    Corregí un poquito el texto de la Outro.
    Agregué el logo y las canciones para Intro y Outro.

---
+++ Characters/Levels/Titlescreen/TitleLabel.gd
@@ -0,0 +1,15 @@
+extends RichTextLabel
+
+const IntroScene = preload("res://Characters/Levels/Intro/IntroScene.tscn".tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var tween = create_tween()
+	tween.tween_property( self, "visible_characters", 1000,20.0)
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if (Input.is_action_pressed("ui_accept")):
+		var tween_screen = create_tween()
+		tween_screen.tween_callback(func(): get_tree().change_scene_to_packed(IntroScene))
