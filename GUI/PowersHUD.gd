91154d56df64339a5e818dbfc3bb1aeb40720309
juanFdS <juan9794@gmail.com>
2023-02-03 22:26:32 -0300

    Fix icono del poder secundario

--- GUI/PowersHUD.gd
+++ GUI/PowersHUD.gd
@@ -2,4 +2,4 @@ extends Control
 
 func _process(delta):
 	$PrimaryPower.texture = Party.primary_power().icon
-	$SecondaryPower.texture = Party.primary_power().icon
+	$SecondaryPower.texture = Party.secondary_power().icon
